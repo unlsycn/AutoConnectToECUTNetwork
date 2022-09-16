3e40fbd0a17bb6c96901dd5555df47b5e57cf653
unlsycn <g.humphrey.sln@gmail.com>
2022-09-16 14:04:03 +0800

    feat: initialize scripts

---
+++ ConnectToCampusNetwork.ps1
@@ -0,0 +1,13 @@
+﻿$UserAccount = ""
+$UserPassword = ""
+$ISP = "" # 中国移动 - cmcc | 中国联通 - unicom | 中国电信 - telecom | 校园网 - 留空
+$NIC = "以太网" # 以太网 | WLAN | ...
+$UserIP = Get-NetIPAddress -InterfaceAlias $NIC -AddressFamily IPv4 | select -exp IPv4Address
+$UserMAC = "0123456789ab"
+$Random = Get-Random -Maximum 9999 -Minimum 1000
+$Uri = "http://172.21.255.105:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=" + $UserAccount + "%40" + $ISP + "&user_password=" + $UserPassword + "&wlan_user_ip=" + $UserIP + "&wlan_user_ipv6=&wlan_user_mac=" + $UserMAC + "&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.3.3&v=" + $Random
+$Headers = @{
+    "Host" = "172.21.255.105:801"
+    "User-Agent" = "Mozilla/5.0 (Windows 98; Win 9x 4.90; en-US; rv:1.9.1.20) Gecko/20160423 Firefox/35.0"
+}
+$Response = Invoke-WebRequest -Uri $Uri -Headers $Headers
---
+++ README.md
@@ -0,0 +1,14 @@
+# 自动登录 ECUT 校园网 For Windows
+
+- 使用 Windows Task Scheduler, 插上网线自动登录（远离那些为了登个校园网就 24h 不间断运行的 Python 脚本吧)
+- 只支持 Windows
+
+## 使用方法
+
+打开`ConnectToCampusNetwork.ps1`，把你的账号密码ISP填进去。
+
+`NIC`填你的网卡名称，中文系统的话插网线一般是「以太网」，WLAN 就是「WLAN」。
+
+把`ConnectToCampusNetwork.ps1`放到`用户目录/Tasks`下（可以在`Connect to Campus Network.xml`中修改）。
+
+`Win-R`运行`taskschd.msc`，点击`导入任务`，将`Connect to Campus Network.xml`导入。
